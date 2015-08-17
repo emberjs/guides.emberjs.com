477e0e13a9b802c32d3a4f97fd6c714d7e09a55d
Michael Kaiser-Nyman <michaelrkn@gmail.com>
2015-08-16 22:13:56 -0700

    consolidate js version updating

--- tasks/update-versions.js
+++ tasks/update-versions.js
@@ -5,6 +5,7 @@
   ```
 */
 var fs = require('fs');
+var path = require('path');
 var entries = fs.readdirSync('snapshots');
 var versions = [];
 
@@ -19,6 +20,16 @@ for (var index = 0, length = entries.length; index < length; index++) {
 
 fs.writeFileSync('snapshots/versions.json', JSON.stringify(versions), { encoding: 'utf8' });
 
+var currentRevision = versions[versions.length - 1];
+
 var divshot = require('../divshot.json');
-divshot['redirects']['/']['url'] = '/' + versions[versions.length - 1];
+divshot['redirects']['/']['url'] = '/' + currentRevision;
 fs.writeFileSync('divshot.json', JSON.stringify(divshot, null, 2), { encoding: 'utf8' });
+
+['common-modern.js', 'common-old-ie.js', 'common-all.js'].forEach(function(javascript) {
+  var searchJsFile = path.join('snapshots/', currentRevision, '/javascripts/' + javascript);
+  fs.readFile(searchJsFile, 'utf8', function(err, data) {
+    var newJs = data.replace(/CURRENT_REVISION_WILL_APPEAR_HERE_WHEN_DEPLOYED/g, currentRevision);
+    fs.writeFile(searchJsFile, newJs, 'utf8');
+  });
+});
