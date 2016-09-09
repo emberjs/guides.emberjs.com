e3a2c6f199027be8fc8e240329eddce6b22df982
David Baker <dbaker@acorncomputersolutions.com>
2016-09-09 12:07:23 -0600

    Updated deploy instructions for staging

--- release.sh
+++ release.sh
@@ -11,4 +11,8 @@ node tasks/update-versions
 #node tasks/publish-search --engine ember-guides --environment production --revision $VERSION --api-key $API_KEY
 git add --all
 git commit -m "Add snapshot for Ember.js revision $VERSION"
+
+# Staging has shifted to the newer Firebase platform
+firebase deploy --project staging
+
 #firebase deploy -f ember-guides-staging
