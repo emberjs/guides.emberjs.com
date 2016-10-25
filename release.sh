2ab03c76fc0f94510c1ff3b9dd66a2543e1c531c
Ricardo Mendes <rokusu@gmail.com>
2016-10-25 12:02:23 +0100

    Add snapshot for Ember.js revision v2.8.0

--- release.sh
+++ release.sh
@@ -9,6 +9,7 @@ cd ../guides.emberjs.com
 mv ../guides/build snapshots/$VERSION
 node tasks/update-versions
 #node tasks/publish-search --engine ember-guides --environment production --revision $VERSION --api-key $API_KEY
+<<<<<<< Updated upstream
 git add --all
 git commit -m "Add snapshot for Ember.js revision $VERSION"
 
@@ -16,3 +17,8 @@ git commit -m "Add snapshot for Ember.js revision $VERSION"
 firebase deploy --project staging
 
 #firebase deploy -f ember-guides-staging
+=======
+#git add --all
+#git commit -m "Add snapshot for Ember.js revision $VERSION"
+# firebase deploy -f ember-guides-staging
+>>>>>>> Stashed changes
