f3995d84fa2ec03a4a4d3f383e67ac43acbfb3ce
Ricardo Mendes <rokusu@gmail.com>
2017-10-10 06:47:48 +0100

    update release script

--- release.sh
+++ release.sh
@@ -10,10 +10,5 @@ bundle exec middleman build
 cd ../guides.emberjs.com
 mv ../guides/build snapshots/$VERSION
 node tasks/update-versions
-#node tasks/publish-search --engine ember-guides --environment production --revision $VERSION --api-key $API_KEY
-# git add snapshots firebase.json
-# git commit -m "Add snapshot for Ember.js revision $VERSION"
 
-# Staging has shifted to the newer Firebase platform
-# firebase deploy -f ember-guides-staging
-# firebase deploy
+# NEEDS TO UPDATE _redirects
