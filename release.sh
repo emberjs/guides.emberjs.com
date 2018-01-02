26f35a899d40e24ed8bd26bbb26b9b1adedbf0ca
Ricardo Mendes <rokusu@gmail.com>
2018-01-02 08:58:01 +0000

    Ensure branch is checked out

--- release.sh
+++ release.sh
@@ -5,7 +5,7 @@ echo Version: $VERSION
 cd ../guides
 git checkout master
 git pull
-git checkout -b $VERSION
+git checkout -b $VERSION || git checkout $VERSION
 bundle exec middleman build
 cd ../guides.emberjs.com
 mv ../guides/build snapshots/$VERSION
